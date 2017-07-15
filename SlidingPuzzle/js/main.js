76100839ba86636b9ccbf1e65632ba6c8a7df047
SpionAtom <tdpb@gmx.de>
2017-07-15 10:19:17 +0200

    moved experiments to sliding puzzle

--- SlidingPuzzle/js/main.js
+++ SlidingPuzzle/js/main.js
@@ -1,67 +1,71 @@
-'use strict';
-
-    // Basic Canvas settings
-    var config = {
-      'resolution': { 'width': 800, 'height': 480 },
-      backgroundColor: 0x9B9B67,
-      framesPerSeconds: 60
-    };
-    var rootStage, renderer;
-
-    // Timer
-    var lastUpdated = 0, updateWaitTime = 1000 / config.framesPerSeconds;
-
-window.onload = init;
-
-function init() {    
-    var render = setup(config);
-    render();    
-}
-
-function setup(_config) {    
-    renderer = new PIXI.CanvasRenderer(_config.resolution.width, _config.resolution.height);
-    renderer.backgroundColor = _config.backgroundColor;
-
-    var canvasBox = document.getElementById('CanvasBox');
-    canvasBox.appendChild(renderer.view);
-    rootStage = new PIXI.Container();
-    build();
-    return render;
-}
-
-function build() {
-    // button actions
-    document.getElementById("reset").addEventListener("click", resetMap);
-    document.getElementById("scramble").addEventListener("click", scrambleMap);
-
-    document.getElementById("desiredWidth").value = 5;
-    document.getElementById("desiredHeight").value = 4;
-
-    var graphics = new PIXI.Graphics();
-    rootStage.addChild(graphics);
-    initGame(rootStage, config);
-
-
-
-}
-
-function render() {
-    update();
-    renderer.render(rootStage);
-    requestAnimationFrame(render);
-}
-
-function update() {
-
-    // update timer
-    var ms = new Date().getTime();
-    if (ms - lastUpdated > updateWaitTime) {
-      lastUpdated = ms;
-    } else {
-      return;
+///<reference path="pixi.js.d.ts" />
+///<reference path="webfontloader.d.ts" />
+///<reference path="./globals.ts" />
+///<reference path="./config.ts" />
+///<reference path="./app.ts" />
+var WindowHandler = (function () {
+    function WindowHandler() {
+        this.onResize = debounce(function () {
+            console.log("- window resize");
+            var padding = 32;
+            var canvasRect = windowHandler.canvas.getBoundingClientRect();
+            var w = window.innerWidth - padding, h = window.innerHeight - canvasRect.top - padding / 2;
+            w = Math.max(w, 240);
+            h = Math.max(h, 240);
+            windowHandler.pixiApp.renderer.resize(w, h);
+            app.onWindowResize(w, h);
+        }, 250);
     }
-
-    // do stuff
-    
-    
+    WindowHandler.prototype.init = function () {
+        console.log("Initialisierung...");
+        this.pixiApp = new PIXI.Application(applicationConfig.resolution.width, applicationConfig.resolution.height, { backgroundColor: applicationConfig.backgroundColor });
+        this.canvas = document.getElementById("CanvasBox");
+        this.canvas.appendChild(this.pixiApp.view);
+        app = new App(this.pixiApp);
+        var padding = 32;
+        var canvasRect = this.canvas.getBoundingClientRect();
+        var w = window.innerWidth - padding, h = window.innerHeight - canvasRect.top - padding / 2;
+        this.pixiApp.renderer.resize(w, h);
+        app.onWindowResize(w, h);
+        document.getElementById('scramble').addEventListener('click', scramble);
+        document.getElementById('reset').addEventListener('click', resetGame);
+    };
+    return WindowHandler;
+}());
+var windowHandler = new WindowHandler();
+var app;
+window.addEventListener('resize', windowHandler.onResize);
+// http://www.enea.sk/blog/preloading-web-font-pixi.js.html
+var fontName = 'Nunito';
+window.onload = function () {
+    document.body.innerHTML = "<span id='loading' style='color: white;'>Loading web font...</span>" + document.body.innerHTML;
+    WebFont.load({
+        // this event is triggered when the fonts have been rendered, see https://github.com/typekit/webfontloader
+        active: function () {
+            // let browser take a breath. Some fonts may require more room for taking deep breath
+            setTimeout(function () {
+                var loading = document.getElementById('loading');
+                loading.parentNode.removeChild(loading);
+                ;
+                windowHandler.init();
+            }, 500);
+        },
+        // when font is loaded do some magic, so font can be correctly rendered immediately after PIXI is initialized
+        fontloading: doMagic,
+        // multiple fonts can be passed here
+        google: {
+            families: [fontName]
+        }
+    });
+};
+// this one is important
+function doMagic() {
+    // create <p> tag with our font and render some text secretly. We don't need to see it after all...
+    var el = document.createElement('p');
+    el.style.fontFamily = fontName;
+    el.style.fontSize = "0px";
+    el.style.visibility = "hidden";
+    el.innerHTML = '.';
+    document.body.appendChild(el);
 }
+;
