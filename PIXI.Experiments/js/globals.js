65cb90eba2c35e68075376881649ea1825e3d664
SpionAtom <tdpb@gmx.de>
2017-07-15 11:42:22 +0200

    just sliding puzzle

--- PIXI.Experiments/js/globals.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-    // Basic Canvas settings
-    var config = {
-      'resolution': { 'width': 800, 'height': 480 },
-      backgroundColor: 0x474758,
-      framesPerSeconds: 60
-    };
-
